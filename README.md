3dce72eb28b47de7886626d2a64599e4a138881b
Hannah S <hannahsartin@gmail.com>
2022-02-11 16:59:22 -0600

    feature changes and fixes, README file update
    Removed check for last.fm api secret & references to it
    Made the variable check errors clearer
    Made array of default small icons
    Made large image the current track's album art
    Made large image text display album information

--- README.md
+++ README.md
@@ -1,22 +1,40 @@
 # Discord Last.fm Script
 
-This is a small script which streams Last.fm now playing data and displays it as a Discord status.
+Script that sets your Discord status with information about what song you're currently scrobbling. Forked from [Draxerra/lastfm-discord-status](https://github.com/Draxerra/lastfm-discord-status) and expanded upon a little.
+
+This is what it looks like:  
+![](https://i.imgur.com/iABW6fq.png)   
+![](https://i.imgur.com/iLkfBj4.png)
 
 ## First Steps
 
-Firstly, you will need to set up a custom rich presence for Last.fm. Go [here](https://discordapp.com/developers/applications/) and create an application. The name of the application will be what is displayed on your status. Make sure to add any art assets for the application under Rich Presence -> Art Assets. These will be displayed on your status.
+1. Create a Discord application [here](https://discordapp.com/developers/applications/). The name of the application is what your status will display as. Grab the Client ID from the OAuth2 section of your app dashboard.
 
-You will also need to get an API account on Last.fm [here](https://www.last.fm/api/account/create).
+2. Get an API account on Last.fm [here](https://www.last.fm/api/account/create) and grab the API key from it.
 
 ## Running the Script
 
-You will need to create a .env file in the root directory and set the following environment variables:
-DISCORD_CLIENT_ID=`Enter your Discord Application Client ID here`  
-LAST_FM_API_KEY=`Enter your Last.fm API key here`  
-LAST_FM_USERNAME=`Enter your Last.fm username here`  
-LARGE_IMAGE_KEY=`Enter your Discord Application Large Image Key here (optional)`  
-LARGE_IMAGE_TOOLTIP=`Enter your Discord Application Large Image Text here (optional)`  
-SMALL_IMAGE_KEY=`Enter your Discord Application Small Image Key here (optional)`  
-SMALL_IMAGE_TOOLTIP=`Enter your Discord Application Small Image Text here (optional)`  
+You will need a .env file in the root directory. Copy example.env and rename it, then set the following environment variables:
 
-Now run `npm start` and Discord will display your custom rich presence status with whatever is currently scrobbling on your Last.fm account! ^_^
+| Variable          |                         Value |
+|:------------------|--------------------------------------:|
+| DISCORD_CLIENT_ID | Discord Application Client ID |
+| LAST_FM_API_KEY   | Last.fm API Key               |
+| LAST_FM_USERNAME  | Your Last.fm username         |
+| SMALL_IMAGE_KEY (Optional)   | `lastfm | youtube | apple | bandcamp| soundcloud | deezer | custom url`  <br/> You can choose from the premade keys or add your own image url.
+| SMALL_IMAGE_TOOLTIP (Optional)| Sets the tooltip text for the small image icon.|
+
+### Built-in image icons
+<details><summary>Expand to view (some of them are big)</summary>
+| Tag          |                                Image |
+|:-------------|-------------------------------------:|
+| lastfm       | ![](https://i.imgur.com/122GWJx.png) |
+| youtube      | ![](https://i.imgur.com/CIZRe5K.png) |
+| apple        | ![](https://i.imgur.com/y0BK5xs.jpg) |
+| bandcamp     | ![](https://i.imgur.com/t7J1yCa.png) |
+| soundcloud   | ![](https://i.imgur.com/bnWWcIz.png) |
+| pandora      | ![](https://i.imgur.com/X8iV8q7.jpg) |
+| deezer       | ![](https://i.imgur.com/ZenqnHA.png) |
+</details>
+
+Run `npm start` and Discord will display your custom rich presence status with whatever you're currently Scrobbling!
--- index.js
+++ index.js
@@ -1,53 +1,64 @@
 require("dotenv").config();
 const RPC = require("discord-rpc");
 const { getEvent, getNowPlaying } = require("./lastfm");
-const client = new RPC.Client({ transport: "ipc" });
+const client = new RPC.Client({transport: "ipc"});
 
-if (!process.env.DISCORD_CLIENT_ID || !process.env.LAST_FM_API_KEY || !process.env.LAST_FM_SECRET || !process.env.LAST_FM_USERNAME) {
-    throw new Error("You must specify a DISCORD_CLIENT_ID, LAST_FM_API_KEY, LAST_FM_SECRET and a LAST_FM_USERNAME environment variable.");
+if (!process.env.DISCORD_CLIENT_ID) {
+    throw new Error("Missing Discord Client ID");
+} else if(!process.env.LAST_FM_API_KEY) {
+    throw new Error("Missing Last.fm API Key");
+} else if(!process.env.LAST_FM_USERNAME) {
+    throw new Error("Missing Last.fm Username")
 }
 
 const clientId = process.env.DISCORD_CLIENT_ID;
+let sImageKey = process.env.SMALL_IMAGE_KEY;
+const sImages = new Map([
+    ["lastfm", "https://i.imgur.com/122GWJx.png"],
+    ["youtube", "https://i.imgur.com/CIZRe5K.png"],
+    ["apple", "https://i.imgur.com/y0BK5xs.jpg"],
+    ["soundcloud", "https://i.imgur.com/bnWWcIz.png"],
+    ["bandcamp", "https://i.imgur.com/t7J1yCa.png"],
+    ["deezer", "https://i.imgur.com/ZenqnHA.png"],
+    ["pandora","https://i.imgur.com/X8iV8q7.jpg"]
+]);
+for(const item of sImages[Symbol.iterator]()) {
+    if (sImageKey == item[0]) {
+        sImageKey = item[1];
+        break
+    }
+}
 
 RPC.register(clientId);
-
-client.on("ready", () => {
+client.on("ready", ()=> {
     let prev;
-    setInterval(async() => {
+    setInterval(async()=>{
         try {
-            // Attempt to get the track now playing on user's Last.fm profile.
             const curr = await getNowPlaying({
                 username: process.env.LAST_FM_USERNAME,
-                apiKey: process.env.LAST_FM_API_KEY,
+                apiKey: process.env.LAST_FM_API_KEY
             });
-            // Get the event currently occuring by comparing the previous track polled to the latest.
-            const { event, track } = getEvent(prev, curr);
-
-            // A new track is now playing, so set the discord status to that track.
-            if (event === "nowPlaying") {
+            const {event, track} = getEvent(prev, curr);
+            if(event === "nowPlaying") {
                 const startTimestamp = new Date();
                 await client.setActivity({
                     details: track.name,
                     state: `by ${track.artist['#text']}`,
                     startTimestamp,
-                    largeImageKey: process.env.LARGE_IMAGE_KEY,
-                    largeImageText: process.env.LARGE_IMAGE_TOOLTIP,
-                    smallImageKey: process.env.SMALL_IMAGE_KEY,
+                    largeImageKey: `${track.image[3]['#text']}`,
+                    largeImageText: `${track.artist['#text']} - ${track.album['#text']}`,
+                    smallImageKey: sImageKey,
                     smallImageText: process.env.SMALL_IMAGE_TOOLTIP,
                     instance: false,
                 });
                 console.log(`Now playing ${track.name} by ${track.artist['#text']}`);
-
-            // Track stopped playing, so clear the Discord status.
-            } else if (event === "stoppedPlaying") {
+            } else if(event === "stoppedPlaying") {
                 await client.clearActivity();
                 console.log(`Stopped playing ${track.name} by ${track.artist['#text']}`);
             }
-
-            // Set the previous track to the current, to be compared to a new track in the next iteration.
             prev = curr;
-        } catch (err) {
-            console.error(err);
+        } catch(e) {
+            console.error(e);
         }
     }, 5000);
 });
